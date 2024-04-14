1b618f1e80a169db6ab40bb00cf6c3115eef4134
hustle-dev <dlwoabsdk@gmail.com>
2024-04-14 23:43:36 +0900

    [rust] 섹션6: 콜렉션 데이터

--- rust/activities/src/bin/a14.rs
+++ rust/activities/src/bin/a14.rs
@@ -11,4 +11,39 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    favorite_color: String,
+}
+
+fn print_name_color(name: &str, color: &str) {
+    println!("name: {:?}, favorite color: {:?}", name, color);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            age: 14,
+            name: String::from("AAA"),
+            favorite_color: String::from("Black"),
+        },
+        Person {
+            age: 31,
+            name: String::from("BBB"),
+            favorite_color: String::from("Blue"),
+        },
+        Person {
+            age: 65,
+            name: String::from("CCC"),
+            favorite_color: String::from("Purple"),
+        }
+    ];
+
+    for person in people {
+        if person.age < 35 {
+            print_name_color(&person.name, &person.favorite_color);
+            println!("age: {:?}", person.age);
+        }
+    }
+}
